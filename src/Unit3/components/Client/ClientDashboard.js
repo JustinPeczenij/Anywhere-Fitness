9ade2f183223a57be794fa875dc134a2c2457257
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-29 11:11:58 -0400

    onboarding feature done for instructor.

--- src/Unit3/components/Client/ClientDashboard.js
+++ src/Unit3/components/Client/ClientDashboard.js
@@ -1,43 +1,34 @@
 import { Route, Link, Switch } from 'react-router-dom';
+import { useEffect } from 'react';
+import 'intro.js/introjs.css';
+import introJs from 'intro.js';
 
-const example = [
-    {
-        date: "Apr 30th 2021",
-        duration: 45,
-        intensitylevel: "BEGINNER",
-        location: "Allentown,  PA",
-        maxsize: 45,
-        name: "Justin K Peczenij",
-        numregisteredattendees: 0,
-        starttime: "6:30 pm",
-        type: "Yoga",
-    },
-    {
-        date: "Apr 30th 2021",
-        duration: 45,
-        intensitylevel: "BEGINNER",
-        location: "Allentown,  PA",
-        maxsize: 45,
-        name: "Justin K Peczenij",
-        numregisteredattendees: 0,
-        starttime: "6:30 pm",
-        type: "Yoga",
-        }
-    
-  ]
   
   export default function ClientDash(props){
-    // return (<>
-    //       <Switch>
-    //         <Route path='/dashboard/client/search'>
-    //           <FindClass />
-    //         </Route>
-    //         <Route path='/dashboard/client'>
-    //           <ClientClasses />
-    //         </Route>
+    const checkForOnboarding = () => {
+        if(window.localStorage.getItem('id_first')){
+            return
+        } else introJs().setOptions({
+            steps: [
+                {intro: "Welcome the Anywhere Fitness! Here you can browse and search for classes, and hopefully make a reservation."},
+                {element: document.querySelector('.create-class-btn'), intro: 'Create a class for prospects to see.'},
+            ]
+        }).start()
+    } 
+    useEffect(() => {
+        checkForOnboarding()
+    }, [])
+    return (<>
+          {/* <Switch>
+            <Route path='/dashboard/client/search'>
+              <FindClass />
+            </Route>
+            <Route path='/dashboard/client'>
+              <ClientClasses />
+            </Route>
     
-    //       </Switch>
-    //     </div>
-    //   )
+          </Switch> */}
+        </>
+      )
     // }
   }
--- src/Unit3/components/InstrucorManageClasses/InstructorClass.js
+++ src/Unit3/components/InstrucorManageClasses/InstructorClass.js
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import EditClass from './EditClass';
 import styled from 'styled-components';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 import { baseURL } from '../../utils/baseURL';
+import 'intro.js/introjs.css';
+import introJs from 'intro.js';
 
 const StyledDiv = styled.div`
   margin: 1%;
@@ -25,6 +27,23 @@ export default function InstructorClass(props) {
         })
         .catch(err => console.log(err))
     }
+    
+    const checkForOnboarding = async () => {
+      if(window.localStorage.getItem('id_first')){
+          return
+      } else {introJs().setOptions({
+          steps: [
+              {element: document.querySelector('.edit-btn'), intro: 'Here you can edit your class or delete if you need.'}
+          ]
+        }).start()
+        //INSTRUCTOR NO LONGER HAS ACCESS TO ONBOARDING
+        await window.localStorage.setItem('id_first', 69)
+        }
+
+    } 
+    useEffect(() => {
+        checkForOnboarding()
+    }, [])
 
     return (
       <StyledDiv>
@@ -34,7 +53,7 @@ export default function InstructorClass(props) {
             <p>Duration: {c.duration} minutes</p>
             <p>Where: {c.location}</p>
             <p>When: {c.date} @ {c.starttime}</p>
-            {isEditing ? null : <button onClick={() => setIsEditing(true)}>edit</button>}
+            {isEditing ? null : <button className='edit-btn' onClick={() => setIsEditing(true)}>edit</button>}
         </div>
         <div>
             {isEditing && <EditClass c={c} setIsEditing={setIsEditing} classes={classes} setClasses={setClasses} deleteClass={deleteClass} instructor={instructor}/>}
